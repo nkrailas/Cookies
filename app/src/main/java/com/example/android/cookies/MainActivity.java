be60a2ddcfeb4164aceac3cf7a14186c72f0f1a6
Nida-Krailas <nkrailas@gmail.com>
2018-02-27 23:17:37 -0600

    Update

--- app/src/main/java/com/example/android/cookies/MainActivity.java
+++ app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -21,12 +21,12 @@ public class MainActivity extends AppCompatActivity {
     public void eatCookie(View view) {
         /** Find a reference to the ImageView in the layout. Change the image. */
 
-        ImageView afterCookie = (ImageView) findViewById(R.id.android_cookie_image_view);
-        afterCookie.setImageResource(R.drawable.after_cookie);
+        ImageView imageView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        imageView.setImageResource(R.drawable.after_cookie);
 
         /** Find a reference to the TextView in the layout. Change the text. */
 
-        TextView fullMessage = (TextView) findViewById(R.id.status_text_view);
-        fullMessage.setText("I'm so full");
+        TextView textView = (TextView) findViewById(R.id.status_text_view);
+        textView.setText("I'm so full");
     }
 }
